51d7f84cf5af51d19311c9e123e943e9e4340ed8
Your Name <you@example.com>
2019-06-07 16:47:28 +0700

    add file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include('circle.php');
+include('cylinder.php');
+
+$circle = new Circle(5, 'red');
+echo 'CIRCLE' . '<br>';
+echo $circle->toString() . '<br>';
+echo 'Perimeter: ' . $circle->getPerimeter() . '<br>';
+echo 'Area: ' . $circle->getArea() . '<br>';
+$cylinder = new Cylinder(10, 'red', 20);
+echo 'CYLINDER' . '<br>';
+echo $cylinder->toString() . '<br>';
+echo 'Perimeter: ' . $cylinder->getPerimeter() . '<br>';
+echo 'Area: ' . $cylinder->getArea();
